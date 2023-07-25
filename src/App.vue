68db3d6af98ad730ee3daeadf23f04d3b0a23519
Franco Di Nitto <dinittofranco@gmail.com>
2023-07-25 02:01:03 -0300

    Maquetado OK, falta tarjeta de quiniela

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-
-
-    </v-app-bar>
+    <v-app-bar app color="primary" dark> </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
---
+++ src/components/VueChart.vue
@@ -0,0 +1,72 @@
+<template>
+  <Doughnut
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :height="height"
+    style="width: 31.25rem; height: 31.25rem"
+  />
+</template>
+  
+  <script>
+import { Doughnut } from "vue-chartjs";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
+export default {
+  name: "VueChart",
+  components: {
+    Doughnut,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: "doughnut-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ["Vane Va", "UxP", "Juntos", "FDI"],
+        datasets: [
+          {
+            label: "Votos",
+            backgroundColor: ["#0C74AE", "#00B8FF", "#FFDA00", "#E00712"],
+            data: [40, 20, 80, 10],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "bottom",
+          },
+        },
+        maintainAspectRatio: false,
+      },
+    };
+  },
+};
+</script>
+  
