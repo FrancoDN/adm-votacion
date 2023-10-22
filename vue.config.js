2ba1dd1ab9f0dd6ae5f659bdce8e0e9cbad6339a
Franco Di Nitto <dinittofranco@gmail.com>
2023-10-22 17:44:12 -0300

    SOLUCIONADO PRDU 22 OCT

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  publicPath: process.env.NODE_ENV === 'production'
-   ? '/adm-votacion/'
-   : '/'
-})
